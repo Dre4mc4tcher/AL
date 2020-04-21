56446df0550a2b4e39a7aeadc7c0e9f02499a200
Dre4mc4tcher <50794512+Dre4mc4tcher@users.noreply.github.com>
2020-04-21 16:45:46 +0200

    Create NeedsHealing.js

---
+++ NeedsHealing.js
@@ -0,0 +1,17 @@
+function needsHealing(){
+    let partyMember = partyMembers();
+    let potentialHealTargets = Object.values(parent.entities).filter(pt => partyMember.includes(pt.name));
+    let partyHealthRatios = [];
+    partyHealthRatios.push({name:character.name,healthRatio:(character.hp/character.max_hp)});
+    for(i =0;i < potentialHealTargets.length;i++){
+        let healthRatio = potentialHealTargets[i].hp/potentialHealTargets[i].max_hp;
+            partyHealthRatios.push({
+                name:potentialHealTargets[i].name,
+                healthRatio:healthRatio,
+            });
+    }
+    partyHealthRatios.sort(function(current,next){
+        return current.healthRatio - next.healthRatio;
+    })
+    return partyHealthRatios[0];
+}
