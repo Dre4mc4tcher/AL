222284000f18fe7625524f4db65be1fbd0d1d23f
Dre4mc4tcher <50794512+Dre4mc4tcher@users.noreply.github.com>
2022-08-19 21:16:06 +0200

    Update Mage001.js

--- Mage001.js
+++ Mage001.js
@@ -591,18 +591,7 @@ function needsHealing(partyMembers){
 
 function partyMembers(){
 
-	people=[];
-	
-	
-var partyList = Object.keys(parent.party_lists);
-for(i = 0; i< partyList.length; i++){
-	let objectkey = partyList[i];
-	let partyListA = Object.keys(parent.party_lists[objectkey]);
-	for(j = 0; j < partyListA.length; j++){
-		people.push(partyListA[j]);
-	}
-}
-
+	people=parent.party_list;
 return people;
 }
 
