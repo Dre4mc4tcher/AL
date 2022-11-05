87b5e78ff9e6e792158b710a76627536b5955ecd
Dre4mc4tcher <50794512+Dre4mc4tcher@users.noreply.github.com>
2022-11-05 12:45:08 +0100

    Update Mage001.js

--- Mage001.js
+++ Mage001.js
@@ -1,10 +1,6 @@
 targetToHunt =[];
 var group = ['Schlange','Spinne','Skorpion','Warrior001','Priest001','Mage001']
-parent.socket.on('hit',function(data){
-if(data.dreturn){
-	console.log(data)
-}
-})
+
 setInterval(function () 
 {
     if (character.name == group[0]) 
@@ -688,3 +684,29 @@ function distance_to_point(x, y) {
 }
 
 
+//Sells items in whitelist
+//Courtesy of: JourneyOver
+
+var sItem = true; //Enable selling of items = true, Disable selling of items = false
+var whitelist = ['stramulet','dexamulet','intamulet','wgloves']; //whitelist is for the selling of items
+
+setInterval(function() {
+
+  //sells items in whitelist
+  if (sItem) {
+    sellItem()
+  }
+
+}, 1000 / 4); //Loop every 1/4 seconds.
+
+function sellItem() {
+  for (let i = 0; i < character.items.length; i++) {
+    let c = character.items[i];
+    if (c) {
+      if (c && whitelist.includes(c.name)) {
+
+        sell(i);
+      }
+    }
+  }
+}
